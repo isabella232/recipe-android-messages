86297bc96032a386e357833f71844621050bc675
Stefan Malzner <stefan@adlk.io>
2019-10-22 14:33:13 +0200

    clear service workers on unload

--- webview.js
+++ webview.js
@@ -1,3 +1,33 @@
+const { remote } = require('electron');
+
+const webContents = remote.getCurrentWebContents();
+const { session } = webContents;
+
+setTimeout(() => {
+  const elem = document.querySelector('#af-error-container');
+  if (elem && elem.innerText.toLowerCase().includes('the requested url was not found on this server')) {
+    window.location.reload();
+  }
+}, 1000);
+
+window.addEventListener('beforeunload', async () => {
+  try {
+    session.flushStorageData();
+    session.clearStorageData({
+      storages: ['appcache', 'serviceworkers', 'cachestorage', 'websql', 'indexdb'],
+    });
+
+    const registrations = await window.navigator.serviceWorker.getRegistrations();
+
+    registrations.forEach((r) => {
+      r.unregister();
+      console.log('ServiceWorker unregistered');
+    });
+  } catch (err) {
+    console.err(err);
+  }
+});
+
 module.exports = (Franz) => {
   function getMessages() {
     const messages = document.querySelectorAll('.text-content.unread').length;
